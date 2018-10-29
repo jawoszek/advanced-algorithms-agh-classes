5b4c6d737c16a052ca9aad579850b0fbff05c170
jawok <kacper.jawoszek@pega.com>
2018-10-29 21:40:12 +0100

    Second classes finished

--- src/main/groovy/FordFulkersonGraph.groovy
+++ src/main/groovy/FordFulkersonGraph.groovy
@@ -8,33 +8,63 @@ class FoldFulkersonGraph {
         double maxFlow = 0
 
         while (path = startingNode.findPathTo(endId)) {
-            println "found path $path"
+//            println "found path $path"
             double currentFlow = Double.MAX_VALUE
 
             for (Connection connection : path.path) {
-                currentFlow = Math.min(currentFlow, connection.possibleFlow())
+                if (path.reverted[connection]) {
+                    currentFlow = Math.min(currentFlow, -connection.backFlow)
+                } else {
+                    currentFlow = Math.min(currentFlow, connection.possibleFlow())
+                }
             }
 
             maxFlow += currentFlow
 
             for (Connection connection : path.path) {
-                connection.flow += currentFlow
-                connection.backFlow -= currentFlow
+                if (path.reverted[connection]) {
+                    connection.flow -= currentFlow
+                    connection.backFlow += currentFlow
+                } else {
+                    connection.flow += currentFlow
+                    connection.backFlow -= currentFlow
+                }
             }
         }
         maxFlow
     }
 
     static void main(String[] args) {
-        int startId = 1
-        int endId = 4
+        int startId = 10
+        int endId = 60
         Graph graph = graphFromFile()
         double foundFlow = algorithm(graph, startId, endId)
         println foundFlow
+
+        maxNode(graph, 10)
+    }
+
+    static void maxNode(Graph graph, int startId) {
+        double max = Double.MIN_VALUE
+        int id = -1
+        Map<Integer, Double> values = new HashMap<>()
+
+        for (Node node: graph.nodes.values()) {
+            double value = algorithm(graph, startId, node.id)
+            values.put(node.id, value)
+
+            if (value > max) {
+                max = value
+                id = node.id
+            }
+        }
+
+        println values
+        println "Max value $max for node $id"
     }
 
     static Graph graphFromFile() {
-        String text = new File('src/main/resources/conflictExampleForClass2.txt').getText()
+        String text = new File('src/main/resources/graphFromClass2.txt').getText()
         List<String> lines = text.split('\n')
         Map<Integer, Node> nodes = [:]
 
@@ -47,6 +77,7 @@ class FoldFulkersonGraph {
             Node connectedNode = nodes.get(idTo, new Node(id: idTo))
             Connection connection = new Connection(from: node, to: connectedNode, capacity: capacity)
             node.connections.add(connection)
+            connectedNode.incomingConnections.add(connection)
 
             nodes.putAll([(id): node, (idTo): connectedNode])
         }
@@ -55,7 +86,8 @@ class FoldFulkersonGraph {
     }
 
     static class Path {
-        List<Connection> path
+        List<Connection> path = []
+        Map<Connection, Boolean> reverted = [:]
 
         @Override
         String toString() {
@@ -80,31 +112,57 @@ class FoldFulkersonGraph {
     static class Node {
         int id
         List<Connection> connections = []
+        List<Connection> incomingConnections = []
 
         Path findPathTo(int nodeId) {
             Set<Node> visited = [this].toSet()
             List<Node> queue = [this]
             Map<Node, Connection> parentConnection = [:]
+            Map<Connection, Boolean> reverted = [:]
             Connection lastConnection = null
 
             while (queue) {
+                boolean ending = false
                 Node current = queue.pop()
-                println current.id
-                println queue
-                for (Connection connection : current.connections) {
-                    Node potential = connection.to
-                    if (visited.contains(potential) || connection.possibleFlow() <= 0) {
-                        continue
-                    }
+//                println current.id
+//                println queue
+                for (Connection connection : current.connections + current.incomingConnections) {
+                    if (current.id == connection.from.id) {
+                        Node potential = connection.to
+                        if (visited.contains(potential) || connection.possibleFlow() <= 0) {
+                            continue
+                        }
 
-                    visited.add(potential)
-                    queue.add(potential)
-                    parentConnection[potential] = connection
+                        visited.add(potential)
+                        queue.add(0, potential)
+                        parentConnection[potential] = connection
+                        reverted[connection] = false
 
-                    if (potential.id == nodeId) {
-                        lastConnection = connection
-                        break
+                        if (potential.id == nodeId) {
+                            lastConnection = connection
+                            break
+                        }
+                    } else {
+                        Node potential = connection.from
+
+                        if (visited.contains(potential) || connection.backFlow >= 0) {
+                            continue
+                        }
+
+                        visited.add(potential)
+                        queue.add(0, potential)
+                        parentConnection[potential] = connection
+                        reverted[connection] = true
+
+                        if (potential.id == nodeId) {
+                            lastConnection = connection
+                            ending = true
+                            break
+                        }
                     }
+                }
+                if (ending) {
+                    break
                 }
             }
 
@@ -113,11 +171,16 @@ class FoldFulkersonGraph {
             }
 
             Connection currentConnection = lastConnection
-            Path path = new Path(path: [])
+            Path path = new Path()
 
             while (currentConnection) {
                 path.path.add(0, currentConnection)
-                currentConnection = parentConnection[currentConnection.from]
+                path.reverted.put(currentConnection, reverted[currentConnection])
+                if (reverted[currentConnection]) {
+                    currentConnection = parentConnection[currentConnection.to]
+                } else {
+                    currentConnection = parentConnection[currentConnection.from]
+                }
             }
 
             path
