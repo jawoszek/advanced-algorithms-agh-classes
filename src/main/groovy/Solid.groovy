ab01ffef1a2688d9a6bde4379530593bda2cba39
Kacper Jawoszek <jawoszek.kacper@gmail.com>
2018-11-18 17:24:36 +0100

    Third class finished

--- src/main/groovy/Solid.groovy
+++ src/main/groovy/Solid.groovy
@@ -4,6 +4,11 @@ class Solid {
     List<Face> faces = []
 
     static void main(String[] args) {
+        Edge testingEdge = new Edge(p1: new Point(x:5, y:1, z:1), p2: new Point(x:6, y:1, z:1))
+        Point testingPoint = new Point(x:8, y:1, z:1)
+
+        println dist(testingEdge, testingPoint)
+//
         Point ep1 = new Point(x: 0, y:0, z:0)
         Point ep2 = new Point(x: 10, y:0, z:0)
         Point p1 = new Point(x: 3, y:1, z:1)
@@ -12,29 +17,29 @@ class Solid {
         Edge e1 = new Edge(p1: ep1, p2: ep2)
         Edge e2 = new Edge(p1: p1, p2: p2)
 
-        println dist(e1, p1)
-        println dist(e1, e2)
+//        println dist(e1, p1)
+//        println dist(e1, e2)
 
         List<Solid> solids1 = solidsFromFile('src/main/resources/simpleSolid.txt')
 
-        println dist(solids1[0], solids1[1])
+//        println dist(solids1[0], solids1[1])
 
         List<Solid> solids2 = solidsFromFile('src/main/resources/solidsFromClass3.txt')
 
-        println dist(solids2[0], solids2[1])
+        println 'dist between solids: ' + dist(solids2[0], solids2[1])
 
         Face face1 = solids1[0].faces[0]
         Face face2 = solids1[1].faces[0]
-        println dist(face1, face2)
-        println face1.edges().collect { it.toString() }
-        println face2.edges().collect { it.toString() }
+        println 'dist between faces: ' + dist(face1, face2)
+//        println face1.edges().collect { it.toString() }
+//        println face2.edges().collect { it.toString() }
 
         Edge edge1 = face1.edges()[0]
         Edge edge2 = face2.edges()[0]
 
-        println edge1
-        println edge2
-        println dist(edge1, edge2)
+//        println edge1
+//        println edge2
+//        println dist(edge1, edge2)
     }
 
     private static List<Solid> solidsFromFile(String filePath) {
@@ -82,30 +87,57 @@ class Solid {
         List<Edge> edges(){
             [new Edge(p1: p1, p2: p2), new Edge(p1: p2, p2: p3), new Edge(p1: p3, p2: p1)]
         }
+
+        List<Edge> edgesAndMiddle() {
+            [new Edge(p1: p1, p2: p2), new Edge(p1: p2, p2: p3), new Edge(p1: p3, p2: p1),
+             new Edge(p1: p1, p2: new Edge(p1: p2, p2: p3).middle()),
+             new Edge(p1: p2, p2: new Edge(p1: p3, p2: p1).middle()),
+             new Edge(p1: p3, p2: new Edge(p1: p1, p2: p2).middle())]
+        }
+
+        List<Point> pointsAndMiddles() {
+            [p1, p2, p3] + edges().collect { it.middle() }
+        }
+
+
+        @Override
+        String toString() {
+            return "{" +
+                    "" + p1 +
+                    ", " + p2 +
+                    ", " + p3 +
+                    '}';
+        }
     }
 
     private static class Edge {
         Point p1, p2
 
 
+        Point middle() {
+            p1.add(p2.add(p1.multiply(-1)).multiply(0.5))
+        }
+
+        boolean containsPoint(Point p) {
+            double small = 0.00000001
+            double length = dist(p1, p2)
+            double distTo1 = dist(p1, p)
+            double distTo2 = dist(p2, p)
+
+            return Math.abs(length - distTo1 - distTo2) < small
+        }
+
         @Override
         public String toString() {
             return "{" +
                     "" + p1 +
                     ", " + p2 +
-                    '}';
+                    '}'
         }
     }
 
     private static class Point {
         double x, y, z
-
-        List<Face> faces
-        List<Edge> edges
-
-        List<Double> coordinates() {
-            [x, y, z]
-        }
 
         Point difference(Point other) {
             new Point(x: x-other.x, y: y-other.y, z: z-other.z)
@@ -133,7 +165,7 @@ class Solid {
 
 
         @Override
-        public String toString() {
+        String toString() {
             return "($x, $y, $z)"
         }
     }
@@ -143,26 +175,15 @@ class Solid {
     }
 
     static double dist(Edge e, Point p) {
-        double x1 = p.x
-        double y1 = p.y
-        double z1 = p.z
+        Point vector = e.p1.add(e.p2.multiply(-1)).multiply(1/dist(e.p1, e.p2))
+        Point vectorToPoint = p.add(e.p2.multiply(-1))
 
-        double x2 = e.p1.x
-        double y2 = e.p1.y
-        double z2 = e.p1.z
-        double x3 = e.p2.x
-        double y3 = e.p2.y
-        double z3 = e.p2.z
-
-        double b = Math.sqrt(Math.pow((x2 - x3), 2)
-                + Math.pow((y2 - y3), 2)
-                + Math.pow((z2 - z3), 2))
-
-        double S = Math.sqrt(Math.pow((y2 - y1) * (z3 - z1) - (z2 - z1) * (y3 - y1), 2) +
-                Math.pow((z2 - z1) * (x3 - x1) - (x2 - x1) * (z3 - z1), 2) +
-                Math.pow((x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1), 2)) / 2
-
-        return 2 * S / b
+        double t = vector.dot(vectorToPoint)
+        Point pointOnLine = e.p2.add(vector.multiply(t))
+        if (!e.containsPoint(pointOnLine)) {
+            return Math.min(dist(p, e.p1), dist(p, e.p2))
+        }
+        dist(p, pointOnLine)
     }
 
     static double dist(Edge e1, Edge e2) {
@@ -235,24 +256,56 @@ class Solid {
         Point two = q0.add(v.multiply(tc))
 
         dist(one, two)
+    }
 
-        // TODO compare approaches
-        // first approach
-//        (p0.difference(q0)).add(((u.multiply(b*e-c*d)).difference(v.multiply(a*e-b*d))).multiply(1/denominator)).length()
-    }
-//
     static double dist(Edge e, Face f) {
-        f.edges().collect { dist(it, e) }.min()
+        f.edgesAndMiddle().collect { dist(it, e) }.min()
     }
-//
+
+    static List<Double> crossProduct(Point v1, Point v2, Point pointOnPlane) {
+        double x = v1.y*v2.z - v1.z*v2.y
+        double y = v1.z*v2.x - v1.x*v2.z
+        double z = v1.x*v2.y - v1.y*v2.x
+
+        double d = -pointOnPlane.x * x - pointOnPlane.y * y - pointOnPlane.z*z
+
+        [x, y, z, d]
+    }
+
+    static double distanceToPlane(Point p, Point v1, Point v2, Point pointOnPlane) {
+        def (double x, double y, double z, double d) = crossProduct(v1, v2, pointOnPlane)
+
+        x * p.x + y * p.y + p.z * z + d / Math.sqrt(x**2 + y**2 + z**2)
+    }
+
     static double dist(Face f, Point p) {
-        f.edges().collect { dist(it, p) }.min()
+        Point v1 = f.p1.difference(f.p2)
+        Point v2 = f.p2.difference(f.p3)
+        Point v3 = f.p3.difference(f.p1)
+        def (double x, double y, double z) = crossProduct(v1, v2, p)
+        Point vector = new Point(x: x, y: y, z: z)
+
+        double distToPlane = Math.abs(distanceToPlane(p, v1, v2, f.p1))
+
+        double distToEdgePlane1 = distanceToPlane(p, v1, vector, f.p1)
+        double distToEdgePlane2 = distanceToPlane(p, v2, vector, f.p2)
+        double distToEdgePlane3 = distanceToPlane(p, v3, vector, f.p3)
+
+        if (distToEdgePlane1 > 0 || distToEdgePlane2 > 0 || distToEdgePlane3 > 0) {
+            def distBetweenEdges = f.edgesAndMiddle().collect { dist(it, p) }
+            return distBetweenEdges.min()
+        }
+
+        distToPlane
     }
-//
+
     static double dist(Face f1, Face f2) {
-        f1.edges().collect { e1 -> f2.edges().collect { e2 -> dist(e1, e2)} }.flatten().min()
+        double dist1 = f1.pointsAndMiddles().collect { dist(f2, it) }.min()
+        double dist2 = f2.pointsAndMiddles().collect { dist(f1, it) }.min()
+
+        Math.min(dist1, dist2)
     }
-//
+
     static double dist(Solid s1, Solid s2) {
         s1.faces.collect { f1 -> s2.faces.collect { f2 -> dist(f1, f2)} }.flatten().min()
     }
